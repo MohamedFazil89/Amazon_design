bc4f1c1b0dfbf93106d7eb11ff594822cf1828e3
Mohamed Fazil <nmohammedfazil790@gmail.com>
2024-05-11 18:07:44 +0530

    update code

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import PropTypes from 'prop-types';
 import './Cart.css';
 
-function Cart(){
-    return(
+function Cart(props) {
+    return (
         <>
-        <h1>Hello from cart</h1>
+        <div className='box'>
+            <h1 className='title'>{props.title}</h1>
+            <div className='imgcon'>
+                <section className='img1'>
+                    <img src={props.src1} alt="img1" />
+                    <p>{props.info1}</p>
+                </section>
+
+                <section className='img2'>
+                    <img src={props.src2} alt="img2" />
+                    <p>{props.info2}</p>
+
+                </section>
+                <section className='img3'>
+                    <img src={props.src3} alt="img3" />
+                    <p>{props.info3}</p>
+                </section>
+                <section className='img4'>
+                    <img src={props.src4} alt="img4" />
+                    <p>{props.info4}</p>
+                </section>
+            </div>
+            <a href='#' className='button'>{props.buttonText}</a>
+        </div>
         </>
-    )
+    );
 }
+
+Cart.propTypes = {
+    title: PropTypes.string.isRequired,
+    src1: PropTypes.string.isRequired,
+    src2: PropTypes.string.isRequired,
+    src3: PropTypes.string.isRequired,
+    src4: PropTypes.string.isRequired,
+    info1: PropTypes.string.isRequired,
+    info2: PropTypes.string.isRequired,
+    info3: PropTypes.string.isRequired,
+    info4: PropTypes.string.isRequired,
+    buttonText: PropTypes.string.isRequired
+};
+
 export default Cart;
--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,16 @@ import Cart from './Components/Cart.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <Cart />
+    <Cart title="Appliances for your home | Up to 55% off"
+      src1='https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-186x116--B08RDL6H79._SY116_CB667322346_.jpg'
+      src2='https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-186x116--B08RDL6H79._SY116_CB667322346_.jpg'
+      src3='https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-186x116--B08RDL6H79._SY116_CB667322346_.jpg'
+      src4='https://images-eu.ssl-images-amazon.com/images/G/31/IMG15/Irfan/GATEWAY/MSO/Appliances-QC-PC-186x116--B08RDL6H79._SY116_CB667322346_.jpg'
+      info1="info1"
+      info2="info2"
+      info3="info3"
+      info4="info4"
+      buttonText='see more'
+    />
   </React.StrictMode>,
 )
